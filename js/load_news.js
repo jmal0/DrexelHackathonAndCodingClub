7710ba6cbccfd2654724f19bf84c89d8c05dab6f
John Maloney <jmal020@gmail.com>
2015-11-07 13:42:06 -0500

    News loading seems to be working

--- js/load_news.js
+++ js/load_news.js
@@ -4,15 +4,19 @@ NUM_ARTICLES = 5; // Load 5 most recent
 
 $.getJSON( "http://jmal0.github.io/DrexelHackathonAndCodingClub/DHACC-files/news.json", function( data ) {
     console.log("pls");
+    items = [];
+    var news_list = $("ol").addClass("news-items");
+    
     for(var i = 0; i < data["reports"].length && i < NUM_ARTICLES; i++){
-        var news_item = "<li class=\"news-item\">" +
-                        "<div class=\"news-content\">" + 
-                        "<h4>" + data["reports"][i]["title"] + "</h4>" +
-                        "<p>" + data["reports"][i]["text"] + "</p>" +
-                        "<small>" + data["reports"][i]["date"] + "</small>" +
-                        "</div>" + 
-                        "</li>";
-        document.getElementById("news-list").appendChild(news_item);
-        
+        var item = "<li class=\"news-item\">" +
+                   "<div class=\"news-content\">" + 
+                   "<h4>" + data["reports"][i]["title"] + "</h4>" +
+                   "<p>" + data["reports"][i]["text"] + "</p>" +
+                   "<small>" + data["reports"][i]["date"] + "</small>" +
+                   "</div>" + 
+                   "</li>";
+        $('#news-list').append(item);     
     }
+
+
 });

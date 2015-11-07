1aadac4154f491e8c9f40c14a8f8ad4ce448f019
John Maloney <jmal020@gmail.com>
2015-11-07 14:01:09 -0500

    Took out unnecessary stuff

--- js/load_news.js
+++ js/load_news.js
@@ -3,10 +3,6 @@
 NUM_ARTICLES = 5; // Load 5 most recent
 
 $.getJSON( "http://jmal0.github.io/DrexelHackathonAndCodingClub/DHACC-files/news.json", function( data ) {
-    console.log("pls");
-    items = [];
-    var news_list = $("ol").addClass("news-items");
-    
     for(var i = 0; i < data["reports"].length && i < NUM_ARTICLES; i++){
         var item = "<li class=\"news-item\">" +
                    "<div class=\"news-content\">" + 
@@ -15,8 +11,6 @@ $.getJSON( "http://jmal0.github.io/DrexelHackathonAndCodingClub/DHACC-files/news
                    "<small>" + data["reports"][i]["date"] + "</small>" +
                    "</div>" + 
                    "</li>";
-        $('#news-list').append(item);     
+        $('#news-list').append(item);
     }
-
-
 });
